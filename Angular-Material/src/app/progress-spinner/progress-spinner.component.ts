ae1e93be83d423a8acfc7b475e1b0389ffbb2fb0
Rodrigo Dimas <rodrigodimas@200dev.com>
2023-02-05 20:37:08 -0300

    desafio progress bar

--- Angular-Material/src/app/progress-spinner/progress-spinner.component.ts
+++ Angular-Material/src/app/progress-spinner/progress-spinner.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { interval, map } from 'rxjs';
-import { takeWhile } from 'rxjs/operators';
+import { concat, interval } from 'rxjs';
+import { take, map, takeWhile, tap } from 'rxjs/operators';
+import { ProgressBarMode } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-progress-spinner',
@@ -9,18 +10,30 @@ import { takeWhile } from 'rxjs/operators';
 })
 export class ProgressSpinnerComponent implements OnInit {
   public loadingPercent = 0;
+  public queryValue = 0;
+  public queryMode: ProgressBarMode = 'query';
   constructor() { }
 
   ngOnInit(): void {
-    this.loadingProgress(500)
+    this.loadingProgress(500, 95)
     .subscribe(i => this.loadingPercent = i);
+
+    concat(
+    interval(2000)
+    .pipe(
+      take(1),
+      tap( _ => (this.queryMode = 'determinate'))
+    ),
+    this.loadingProgress(500, 100)
+    ).subscribe(i => this.queryValue = i);
+
   }
 
-  loadingProgress(speed: number) {
+  loadingProgress(speed: number, takeUntil: number) {
     return interval(speed)
     .pipe(
       map(i => i * 5),
-      takeWhile(i => i <= 95)
+      takeWhile(i => i <= takeUntil)
     )
    }
 
