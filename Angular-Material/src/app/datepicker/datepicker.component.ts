07928c0e739b259c58ad9116ad8731011068f0f9
Rodrigo Dimas <rodrigodimas@200dev.com>
2023-02-06 21:44:44 -0300

    date picker

---
+++ Angular-Material/src/app/datepicker/datepicker.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Platform } from '@angular/cdk/platform'
+//import * as moment from 'moment';
+
+@Component({
+  selector: 'app-datepicker',
+  templateUrl: './datepicker.component.html',
+  styleUrls: ['./datepicker.component.scss']
+})
+export class DatepickerComponent implements OnInit {
+
+  //platform
+  public startDate = new Date(2022, 9 , 28)
+  public minDate = new Date(2022, 8 , 28)
+  public maxDate = new Date(2022, 11 , 28)
+
+  constructor(private platform: Platform) { }
+
+  ngOnInit(): void {
+  }
+
+  get isTouchDevice() {
+    return this.platform.ANDROID || this.platform.IOS;
+  }
+}
